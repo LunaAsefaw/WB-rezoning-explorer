3311e20a5f259ee0e65fcb81058abbd6907999bb
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2023-01-18 13:50:57 +0100

    Add button to export used filters

--- app/assets/scripts/components/explore/export/csv.js
+++ app/assets/scripts/components/explore/export/csv.js
@@ -5,7 +5,48 @@ import { getTimestamp, round } from '../../../utils/format';
 import config from '../../../config';
 const { indicatorsDecimals } = config;
 
-export default async function exportZonesCsv(selectedArea, zones) {
+export async function exportFiltersCsv(selectedArea, filters) {
+  const doc = format({ headers: true });
+
+  const stream = doc.pipe(blobStream());
+
+  // Parse zones
+  const rows = Object.entries(filters)
+    .map(([filter_id, filter]) => {
+      let filter_row = {
+        id: filter.id,
+        title: filter.title,
+        description: filter.description,
+        secondary_description: filter.secondary_description,
+        category: filter.category,
+        secondary_category: filter.secondary_category,
+        layer: filter.layer,
+        unit: filter.unit,
+        active: filter.active,
+        isRange: filter.isRange,
+        value: !filter.isRange ? filter.input.value : undefined,
+        min_value: filter.isRange ? filter.input.value.min : undefined,
+        max_value: filter.isRange ? filter.input.value.max : undefined,
+      };
+      return filter_row;
+    });
+
+  // Add filters to CSV
+  rows.forEach((z) => doc.write(z) );
+
+
+  doc.end();
+
+  return stream.on('finish', () => {
+    saveAs(
+      stream.toBlob('text/plain;charset=utf-8'),
+      `WBG-REZoning-${selectedArea.id}-filters-${getTimestamp()}.csv`
+    );
+  });
+}
+
+export async function exportZonesCsv(selectedArea, zones) {
+
   const doc = format({ headers: true });
 
   const stream = doc.pipe(blobStream());
