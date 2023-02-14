45632063ea13e34715cccc2ccde4ff7cd4098a55
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2023-02-14 22:55:51 +0100

    github feedback form inital implementation

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -9,6 +9,8 @@ export default {
   mbToken:
     'pk.eyJ1Ijoid2JnLWNkcnAiLCJhIjoiY2l1Z3pxZDVwMDBxcDMzcDJjYmRpYnBicSJ9.hjlLP5TEVhqbTwzhFA1rZw',
   apiEndpoint: 'https://d2b8erzy6y494p.cloudfront.net/v1',
+  githubIssuesApi: 'https://api.github.com/repos/NEDJIMAbelgacem/github_rest_test/issues',
+  githubIssuesAccessToken: 'Bearer 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',
   rawDataDownloadTimeout: 300000, // 5 min
   rawDataDownloadCheckInterval: 2000, // 2 sec
   indicatorsDecimals: {
--- app/assets/scripts/config/staging.js
+++ app/assets/scripts/config/staging.js
@@ -2,5 +2,7 @@
 
 export default {
   environment: 'staging',
-  apiEndpoint: 'https://cenpsr6h9g.execute-api.us-east-2.amazonaws.com/v1'
+  apiEndpoint: 'https://cenpsr6h9g.execute-api.us-east-2.amazonaws.com/v1',
+  githubIssuesApi: 'https://api.github.com/repos/NEDJIMAbelgacem/github_rest_test/issues',
+  githubIssuesAccessToken: 'Bearer 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',
 };
