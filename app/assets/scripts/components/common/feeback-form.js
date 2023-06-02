af3bc04526a17b8847b34756c809c93986cab85f
akshay2408 <akshaybabar2408@gmail.com>
2023-06-02 10:38:54 +0530

    Resolve conflicts

--- app/assets/scripts/components/common/feeback-form.js
+++ app/assets/scripts/components/common/feeback-form.js
@@ -4,55 +4,39 @@ import { withRouter } from 'react-router-dom';
 import Dropdown, { DropTitle } from './dropdown';
 import Button from '../../styles/button/button';
 import SubmitIssueTray from '../explore/submit-issue-tray';
-import styled from 'styled-components';
-
-const ButtonDiv = styled.span`
-  width: 3rem;
-  height: 3rem;
-  button {
-    width: 3rem;
-    height: 3rem;
-  }
-  button::before {
-    margin-top: -5px;
-    margin-left: -5px;
-  }
-`;
 
 function FeedbackForm() {
   return (
     <Dropdown
-      className='feedback'
       alignment='right'
       direction='down'
       triggerElement={
-        <ButtonDiv>
-          <Button
-            id='toggle-feedback-tray'
-            variation='achromic-plain'
-            title='Toggle Feedback Form'
-            hideText
-            useIcon='feedback'
-          >
-            <span>Feedback Form</span>
-          </Button>
-        </ButtonDiv>
+        <Button
+          variation='achromic-plain'
+          title='Toggle Feedback Form'
+          hideText
+          useIcon='feedback'
+        >
+          <span>Feedback Form</span>
+        </Button>
       }
     >
-      <span style={{ color: 'white' }}>Feedback Form</span>
-      <div
-        style={{
-          align: 'center',
-          display: 'grid',
-          backgroundColor: '#23a7f5'
-        }}
-      >
-        <SubmitIssueTray
-          show={true}
-          className='submit-issue-tray'
-          style={{ padding: '0' }}
-        />
-      </div>
+      <DropTitle>Feedback Form</DropTitle>
+      <PanelBlockFooter>
+        <div
+          style={{
+            align: 'center',
+            display: 'grid',
+            backgroundColor: '#23a7f5'
+          }}
+        >
+          <SubmitIssueTray
+            show={true}
+            className='submit-issue-tray'
+            style={{ padding: '0' }}
+          />
+        </div>
+      </PanelBlockFooter>
     </Dropdown>
   );
 }
